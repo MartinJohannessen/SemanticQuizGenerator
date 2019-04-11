b159caaaa01c5fcd7f26e9fa7d4a07869e85f0e7
Martin Johannessen <martin.jodd@gmail.com>
2019-04-11 15:33:13 +0200

    Fixed JSON parser and removed external archives, we have the jena drivers as a library

--- src/semanticQuizGenerator/QuizGeneratorMain.java
+++ src/semanticQuizGenerator/QuizGeneratorMain.java
@@ -1,9 +1,17 @@
 package semanticQuizGenerator;
 
-public class QuizGeneratorMain {
 
+public class QuizGeneratorMain {
+	
 	public static void main(String[] args) {
+	
 		CreateModel model = new CreateModel();
+		
+		String capitalsJSON = JSONParser.readJson("src/data/capitals.json");
+		System.out.println(capitalsJSON);
+		
+		
+		
 	}
 }
 
