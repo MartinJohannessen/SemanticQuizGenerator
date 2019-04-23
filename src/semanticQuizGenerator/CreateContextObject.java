12abb9d429134b415bfd2da223a66261599780ef
torstol <torstols@gmail.com>
2019-04-23 12:02:08 +0200

    Added code to create context objects for resources and properties

--- src/semanticQuizGenerator/CreateContextObject.java
+++ src/semanticQuizGenerator/CreateContextObject.java
@@ -6,8 +6,48 @@ import java.util.Map;
 public class CreateContextObject {
 	CreateModel model = new CreateModel();
 	
-	Map contextObj = new LinkedHashMap() {{
-        put("label", "iri" );
-}};
+	String iriBase = "http://www.wikidata.org/wiki/";
+	
+	// Create context object for the resources
+	Map resContextObj = new LinkedHashMap() {{
+        put("country", iriBase + "Q6256");
+        put("capital", iriBase + "Q5119");
+        put("continent", iriBase + "Q5107");
+        put("highestPoint", iriBase + "Q3393392");
+        put("altitude", iriBase + "Q190200");
+        put("population", iriBase + "Q2625603");
+        put("area", iriBase + "Q11500");
+        put("government", iriBase + "Q7188");
+        put("lifeExpectancy", iriBase + "Q188419");
+        put("bodyOfWater", iriBase + "Q15324");
+        put("headOfState", iriBase + "Q48352");
+        put("waterPercent", iriBase + "Water percent");
+        put("GDP", iriBase + "Q12638");
+        put("income", iriBase + "Q1527264");
+        put("inception", iriBase + "Q24574747");
+        put("airport", iriBase + "Q1248784");        
+	}};
+	
+	String propertyBase = "https://www.wikidata.org/wiki/Property:";
+
+	// Create context object for the properties
+	Map propContextObj = new LinkedHashMap() {{
+		put("capital", propertyBase + "P36");
+		put("continent", propertyBase + "P30");
+		put("highest", propertyBase + "P610");
+		put("altitude", propertyBase + "P2044");
+		put("population", propertyBase + "P1082");
+		put("area", propertyBase + "P2046");
+		put("government", propertyBase + "P122");
+		put("lifeExpectancy", propertyBase + "P2250");
+		put("locatedNextToWater", propertyBase + "P206");
+		put("headofState", propertyBase + "P35");
+		put("sharesBorderWith", propertyBase + "P47");
+		put("waterPercent", propertyBase + "P2927");
+		put("GDP", propertyBase + "P2132");
+		put("income", propertyBase + "P3529");
+		put("inception", propertyBase + "P571");
+	}};
+
 
 }
