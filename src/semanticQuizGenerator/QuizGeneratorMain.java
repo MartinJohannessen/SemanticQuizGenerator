0b3ae3af1353cde744f7218cc9da7020a5c593c3
Martin Johannessen <martin.jodd@gmail.com>
2019-04-23 09:04:30 +0200

    Classpath change?

--- src/semanticQuizGenerator/QuizGeneratorMain.java
+++ src/semanticQuizGenerator/QuizGeneratorMain.java
@@ -7,16 +7,28 @@ import com.github.jsonldjava.utils.JsonUtils;
 
 public class QuizGeneratorMain {
 	
-	public static void main(String[] args) throws JsonParseException, IOException {
+	public static void main(String[] args) {
 	
 		CreateModel model = new CreateModel();
 		
 		String capitalsJSON = JSONParser.readJson("src/data/capitals.json");
+		Object capitalsJsonObj = null;
 		
-		 Object capitalsJsonObj = JsonUtils.fromString(capitalsJSON);
+		 try {
+			capitalsJsonObj = JsonUtils.fromString(capitalsJSON);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-		
-		
+	
+		 try {
+			System.out.println(JsonUtils.toPrettyString(capitalsJsonObj));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 		
 	}
 }
