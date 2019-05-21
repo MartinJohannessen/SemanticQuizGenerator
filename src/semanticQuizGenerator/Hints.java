d8c6e8bed996f02f2621029086d2dc7bd2b11a3c
Martin Johannessen <martin.jodd@gmail.com>
2019-05-21 15:12:47 +0200

    add point system

--- src/semanticQuizGenerator/Hints.java
+++ src/semanticQuizGenerator/Hints.java
@@ -45,7 +45,7 @@ public class Hints {
     	countryQuery(countryIRI, "https://www.wikidata.org/wiki/Property:P2132", "GDP");
     	countryQuery(countryIRI, "https://www.wikidata.org/wiki/Property:P3529", "income");
     	countryQuery(countryIRI, "https://www.wikidata.org/wiki/Property:P36", "capital");
-    	countryQuery(countryIRI, "http://www.semanticQuizGenerator.org/rankByArea", "rank by area");
+    	countryQuery(countryIRI, "http://www.semanticQuizGenerator.org/rankByArea", "world ranking by area");
 
     	countryQuery(capitalIRI, "https://www.wikidata.org/wiki/Property:P1082", "population of the capital");
     	countryQuery(capitalIRI, "https://www.wikidata.org/wiki/Property:P2044", "altitude of the highest point of the capital");
--- src/semanticQuizGenerator/QuizGeneratorMain.java
+++ src/semanticQuizGenerator/QuizGeneratorMain.java
@@ -2,21 +2,33 @@ package semanticQuizGenerator;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 import com.fasterxml.jackson.core.JsonGenerationException;
 
 public class QuizGeneratorMain {
 
 	public static void main(String[] args) throws JsonGenerationException, IOException {
-
-
-		//TerminalQuiz quiz = new TerminalQuiz("http://www.wikidata.org/entity/Q148");
-		//TerminalQuiz quiz2 = new TerminalQuiz("http://www.wikidata.org/entity/Q20");
-		//TerminalQuiz quiz3 = new TerminalQuiz("http://www.wikidata.org/entity/Q17");
-		//TerminalQuiz quiz4 = new TerminalQuiz("http://www.wikidata.org/entity/Q16");
 		
-		ArrayList<String> countries = QuizSessionGenerator.Session(5);
+		System.out.println("Please enter the number of countries you would like in your quiz: ");
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		int nr = reader.nextInt(); // Scans the next token of the input as an int.
+
+
+		System.out.println("RULES: ");
+		System.out.println("Enter your answer below. It's not case sensitive.");
+		System.out.println("If you enter the wrong answer, or nothing, a new hint will be provided.\n");
+		System.out.println("For each hint you get, you will be subtracted one point.");
+		System.out.println("Only the full official name of a country is recognized.");
+		System.out.println("Example: 'pEopLes Republic of ChinA' is correct, 'China' is not.\n");
+		
+		int points = 0;
+		ArrayList<String> countries = QuizSessionGenerator.Session(nr);
 		for (String s: countries) {
 			TerminalQuiz quiz = new TerminalQuiz(s);
+			points+=quiz.getPoints();
 		}
+		
+		System.out.println("You have finished your rounds with "+points+" points");
 	}
 }
